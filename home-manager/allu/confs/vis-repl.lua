132ca82df44c11ba80a861a0ab822db13d36b11b
allumik <alvinmeltsov@gmail.com>
2025-10-29 21:14:49 +0100

    Some further improvements the vis repl

--- home-manager/allu/confs/vis-repl.lua
+++ home-manager/allu/confs/vis-repl.lua
@@ -1,5 +1,5 @@
 -- vis-repl: A module for interacting with a tmux REPL.
--- make it into a plugin!
+-- TODO: make it into a plugin!
 
 local M = {}
 
@@ -8,6 +8,21 @@ function M.get_target_pane(win)
     return win.repl_target_pane
   end
   return "'+'"
+end
+
+function M.prep_text_bracketed(win)
+  local target = M.get_target_pane(win)
+  if not vis.win.selection then
+    vis:info("ERROR: repl-block requires a visual selection.")
+    return
+  end
+
+  -- 1. `>` pipes the visual selection from vis.
+  -- 2. `tmux load-buffer -` loads stdin into a tmux buffer.
+  -- 3. `;` separates the shell commands.
+  -- 4. `tmux paste-buffer ...` pastes the buffer into the target pane.
+  --    "-d" Delete buffer after pasting; "-p" Use bracketed paste mode
+  return '> tmux load-buffer - ; tmux paste-buffer -d -p -t ' .. target
 end
 
 vis:command_register("repl-set", function(argv, _, win)
@@ -29,11 +44,11 @@ vis:command_register("repl-send", function(argv, _, win)
     return
   end
 
+  -- Send multiline text line-by-line
   -- 1. `>` pipes the visual selection to stdin.
   -- 2. `sed ...`: Appends a literal `\Enter` to each line for tmux.
   -- 3. `tr "\\n" "\\0"`: Replaces newlines with null characters for safe handling.
   -- 4. `xargs -0 ...`: Executes `tmux send-keys` with the null-separated input.
-  -- !NB this sends multiline line-by-line, use `repl-block` for bracketed paste
   local cmd = '' ..
     '> sed \'s/;$/\\\\\\;/g; s/\\(.*\\)/\\1\\\\nEnter/\' ' ..
     '  | tr "\\\\n" "\\0" ' ..
@@ -42,20 +57,16 @@ vis:command_register("repl-send", function(argv, _, win)
 end)
 
 -- Sends the selected text using tmux buffers and bracketed paste mode.
-vis:command_register("repl-block", function(argv, _, win)
-  local target = M.get_target_pane(win)
-  if not vis.win.selection then
-    vis:info("ERROR: repl-block requires a visual selection.")
-    return
-  end
+vis:command_register("repl-block", 
+  function(argv, _, win) vis:command(M.prep_text_bracketed(win)) 
+end)
 
-  -- 1. `>` pipes the visual selection from vis.
-  -- 2. `tmux load-buffer -` loads stdin into a tmux buffer.
-  -- 3. `;` separates the shell commands.
-  -- 4. `tmux paste-buffer ...` pastes the buffer into the target pane.
-  --    "-d" Delete buffer after pasting; "-p" Use bracketed paste mode
-  local cmd = '> tmux load-buffer - ; tmux paste-buffer -d -p -t ' .. target
-  vis:command(cmd)
+-- Sends the selected text using tmux buffers and sends an Enter to run the command
+vis:command_register("repl-block-run", function(argv, _, win)
+  local target = M.get_target_pane(win)
+  vis:command(
+    M.prep_text_bracketed(win) .. '; tmux send-keys -t ' .. target .. ' Enter'
+    )
 end)
 
 -- Sends the given text to the REPL target pane.
