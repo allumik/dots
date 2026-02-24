48fc38d86e867b675107b6592491dacd517094cf
allumik <alvinmeltsov@gmail.com>
2026-02-25 00:36:32 +0100

    migrated confs after HM rehaul

---
+++ nixos/users/allu/confs/shell.nix
@@ -0,0 +1,130 @@
+{ config, lib, pkgs, ... }:
+
+let
+  sessvars = {
+    # put dotfiles first!
+    # LC_COLLATE = "C";
+  };
+
+  ## for both zsh and bash
+  aliases = {
+    cp      = "cp -i";     # confirm before overwriting something
+    df      = "df -h";     # human-readable sizes
+    free    = "free -m"; # show sizes in MB
+    np      = "nano -w PKGBUILD";
+    more    = "less";
+    ls      = "ls --color=auto";
+    la      = "ls -A";
+    ll      = "${pkgs.eza}/bin/eza -alF";
+
+    tux     = "${pkgs.tmux}/bin/tmux new-session -A -s main";
+    of      = "xdg-open '$(fzf --preview '${pkgs.bat} {}')'";
+    es      = "$EDITOR '$(fzf --preview '${pkgs.bat} {}')'";
+
+    cdwin   = "cd /mnt/c/Users/alvin";
+    wv      = "wslview";
+  };
+
+  xdgDataDirs = "export XDG_DATA_DIRS=$HOME/.nix-profile/share:$HOME/.nix-profile/share/applications:$XDG_DATA_DIRS";
+in
+{
+  programs = {
+
+    bash = {
+      enable = true;
+
+      profileExtra = xdgDataDirs;
+      bashrcExtra = ''
+        # set a fancy prompt (non-color, unless we know we "want" color)
+        case "$TERM" in
+            xterm-color|*-256color) color_prompt=yes;;
+        esac
+
+        if [ "$color_prompt" = yes ]; then
+            PS1=' ''${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '
+        else
+            PS1=' ''${debian_chroot:+($debian_chroot)}\u@\h:\w\$ '
+        fi
+        unset color_prompt force_color_prompt
+
+        # If this is an xterm set the title to user@host:dir
+        case "$TERM" in
+        xterm*|rxvt*)
+            PS1="\[\e]0;''${debian_chroot:+($debian_chroot)}\u@\h: \w\a\]$PS1"
+            ;;
+        *)
+            ;;
+        esac
+
+        # enable color support of ls and also add handy aliases
+        if [ -x /usr/bin/dircolors ]; then
+            test -r ~/.dircolors && eval "$(dircolors -b ~/.dircolors)" || eval "$(dircolors -b)"
+        fi
+      '';
+      sessionVariables = sessvars;
+      shellAliases = aliases;
+    };
+
+    zsh = {
+      enable = true;
+
+      autosuggestion.enable = true;
+      history.extended = true;
+
+      profileExtra = xdgDataDirs;
+      sessionVariables = sessvars;
+      shellAliases = aliases;
+      plugins = [
+        {
+          name = "fast-syntax-highlighting";
+          file = "fast-syntax-highlighting.plugin.zsh";
+          src = "${pkgs.zsh-fast-syntax-highlighting}/share/zsh/site-functions";
+        }
+        {
+          name = "zsh-nix-shell";
+          file = "nix-shell.plugin.zsh";
+          src = "${pkgs.zsh-nix-shell}/share/zsh-nix-shell";
+        }
+        {
+          name = "zsh-fzf-tab";
+          file = "fzf-tab.plugin.zsh";
+          src = "${pkgs.zsh-fzf-tab}/share/fzf-tab";
+        }
+      ];
+
+      oh-my-zsh = {
+        enable = true;
+        theme = "nicoulaj";
+        plugins = [
+          "fzf"
+          "git"
+          "git-extras"
+          "ssh-agent"
+          "extract"
+          "web-search"
+        ];
+      };
+    };
+
+    fzf = {
+      enable = true;
+      enableZshIntegration = true;
+
+      defaultOptions = [ "--height 40%" ];
+      fileWidgetOptions = [ "--preview 'bat {}'" ];
+      changeDirWidgetOptions = [ "--preview 'bat {}'" ];
+    };
+
+    bat = {
+      enable = true;
+      # This should pick up the correct colors for the generated theme. Otherwise
+      # it is possible to generate a custom bat theme to ~/.config/bat/config
+      config = {
+        theme = "base16";
+        color = "always";
+        style = "numbers";
+        line-range = ":500";
+      };
+    };
+  };
+}
---
+++ nixos/users/allu/home.nix
@@ -0,0 +1,50 @@
+{ config, pkgs, ... }:
+
+{
+  targets.genericLinux.enable = true;
+
+  # Import and source other configuration files
+  imports = [
+    ./confs/shell.nix
+    ./confs/tmux.nix
+  ];
+
+  xdg = {
+    enable = true;
+    mime.enable = true;
+    configFile = {
+      "nvim/init.lua".source = ./confs/nvim.lua;
+      "lf/lfrc".source = ./confs/lfrc;
+      "euporie/config.json".source = ./confs/euporie.json;
+    };
+  };
+
+
+  # General home-manager settings
+  home = {
+    # Home Manager needs a bit of information about you and the
+    # paths it should manage.
+    username = "allu";
+    homeDirectory = "/home/allu";
+
+    # * The PATH for me *
+    sessionPath = [ 
+      "$HOME/.nix-profile/bin"
+      "$HOME/.local/bin" 
+    ];
+
+  packages = with pkgs; [
+    ## Tools & Shells
+    # some minuscle stuff for python/R environments
+    libssh libxml2 libpng libxslt libtiff cairo  # R needs this
+    # terminal bling
+    zsh zsh-nix-shell zsh-fast-syntax-highlighting zsh-fzf-tab
+  ];
+
+    # You should not change this value, even if you update Home Manager.
+    stateVersion = "26.05";
+  };
+  # Notifications about home-manager news
+  news.display = "silent";
+}
+ 
